63880d0b557ce19de917855b715ee82b9919b170
KanadeBlue dw <Jaidenurity@gmail.com>
2024-09-30 19:31:59 +0000

    LevelPacket removed stuff and add stuff

--- src/LevelEventPacket.php
+++ src/LevelEventPacket.php
@@ -10,50 +10,52 @@
  * (at your option) any later version.
  */
 
-declare(strict_types=1);
+ declare(strict_types=1);
 
-namespace pocketmine\network\mcpe\protocol;
-
-use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
-use pocketmine\network\mcpe\protocol\types\LevelEvent;
-
-class LevelEventPacket extends DataPacket implements ClientboundPacket{
-	public const NETWORK_ID = ProtocolInfo::LEVEL_EVENT_PACKET;
-
-	/** @see LevelEvent */
-	public int $eventId;
-	public int $eventData;
-	public ?Vector3 $position = null;
-
-	/**
-	 * @generate-create-func
-	 */
-	public static function create(int $eventId, int $eventData, ?Vector3 $position) : self{
-		$result = new self;
-		$result->eventId = $eventId;
-		$result->eventData = $eventData;
-		$result->position = $position;
-		return $result;
-	}
-
-	public static function standardParticle(int $particleId, int $data, Vector3 $position) : self{
-		return self::create(LevelEvent::ADD_PARTICLE_MASK | $particleId, $data, $position);
-	}
-
-	protected function decodePayload(PacketSerializer $in) : void{
-		$this->eventId = $in->getVarInt();
-		$this->position = $in->getVector3();
-		$this->eventData = $in->getVarInt();
-	}
-
-	protected function encodePayload(PacketSerializer $out) : void{
-		$out->putVarInt($this->eventId);
-		$out->putVector3Nullable($this->position);
-		$out->putVarInt($this->eventData);
-	}
-
-	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handleLevelEvent($this);
-	}
-}
+ namespace pocketmine\network\mcpe\protocol;
+ 
+ use pocketmine\math\Vector3;
+ use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+ use pocketmine\network\mcpe\protocol\types\LevelEvent;
+ 
+ class LevelEventPacket extends DataPacket implements ClientboundPacket{
+	 public const NETWORK_ID = ProtocolInfo::LEVEL_EVENT_PACKET;
+ 
+	 public readonly int $eventId;
+	 public readonly int $eventData;
+	 public readonly Vector3 $position;
+ 
+	 public static function create(int $eventId, int $eventData, Vector3 $position) : self{
+		 $result = new self;
+		 $result->eventId = $eventId;
+		 $result->eventData = $eventData;
+		 $result->position = $position;
+		 return $result;
+	 }
+ 
+	 public static function standardParticle(int $particleId, int $data, Vector3 $position) : self{
+		 $result = new self;
+		 $result->eventId = LevelEvent::ADD_PARTICLE_MASK | $particleId;
+		 $result->eventData = $data;
+		 $result->position = $position;
+		 return $result;
+	 }
+ 
+	 protected function decodePayload(PacketSerializer $in) : void{
+		 $this->eventId = $in->getVarInt();
+		 $this->position = $in->getVector3();
+		 $this->eventData = $in->getVarInt();
+	 }
+ 
+	 protected function encodePayload(PacketSerializer $out) : void{
+		 $out->putVarInt($this->eventId);
+		 // why this nullable?
+		 $out->putVector3($this->position);
+		 $out->putVarInt($this->eventData);
+	 }
+ 
+	 public function handle(PacketHandlerInterface $handler) : bool{
+		 return $handler->handleLevelEvent($this);
+	 }
+ }
+ 
