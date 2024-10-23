bfe616f244d6bc7560d9e6761f252bf395ee5136
KanadeBlue <Jaidenurity@gmail.com>
2024-10-23 04:23:04 +0100

    35.0.0+bedrock-1.21.40

--- src/types/StructureEditorData.php
+++ src/types/StructureEditorData.php
@@ -1,32 +1,82 @@
 <?php
 
-/*
- * This file is part of BedrockProtocol.
- * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
- *
- * BedrockProtocol is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
-
 declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;
 
 class StructureEditorData{
-	public const TYPE_DATA = 0;
-	public const TYPE_SAVE = 1;
-	public const TYPE_LOAD = 2;
-	public const TYPE_CORNER = 3;
-	public const TYPE_INVALID = 4;
-	public const TYPE_EXPORT = 5;
+    public const TYPE_DATA = 0;
+    public const TYPE_SAVE = 1;
+    public const TYPE_LOAD = 2;
+    public const TYPE_CORNER = 3;
+    public const TYPE_INVALID = 4;
+    public const TYPE_EXPORT = 5;
 
-	public string $structureName;
-	public string $structureDataField;
-	public bool $includePlayers;
-	public bool $showBoundingBox;
-	public int $structureBlockType;
-	public StructureSettings $structureSettings;
-	public int $structureRedstoneSaveMode;
+    public string $structureName;
+    public string $structureDataField;
+    public bool $includePlayers;
+    public bool $showBoundingBox;
+    public int $structureBlockType;
+    public StructureSettings $structureSettings;
+    public int $structureRedstoneSaveMode;
+
+    public function __construct(
+        string $structureName = "",
+        string $structureDataField = "",
+        bool $includePlayers = false,
+        bool $showBoundingBox = true,
+        int $structureBlockType = self::TYPE_DATA,
+        StructureSettings $structureSettings = null,
+        int $structureRedstoneSaveMode = 0
+    ) {
+        $this->structureName = $structureName;
+        $this->structureDataField = $structureDataField;
+        $this->includePlayers = $includePlayers;
+        $this->showBoundingBox = $showBoundingBox;
+        $this->structureBlockType = $structureBlockType;
+        $this->structureSettings = $structureSettings ?? new StructureSettings();
+        $this->structureRedstoneSaveMode = $structureRedstoneSaveMode;
+    }
+
+    public function isValidStructureBlockType(): bool {
+        return in_array($this->structureBlockType, [
+            self::TYPE_DATA,
+            self::TYPE_SAVE,
+            self::TYPE_LOAD,
+            self::TYPE_CORNER,
+            self::TYPE_INVALID,
+            self::TYPE_EXPORT
+        ], true);
+    }
+
+    public function getBlockTypeAsString(): string {
+        switch ($this->structureBlockType) {
+            case self::TYPE_DATA:
+                return "Data Block";
+            case self::TYPE_SAVE:
+                return "Save Block";
+            case self::TYPE_LOAD:
+                return "Load Block";
+            case self::TYPE_CORNER:
+                return "Corner Block";
+            case self::TYPE_INVALID:
+                return "Invalid Block";
+            case self::TYPE_EXPORT:
+                return "Export Block";
+            default:
+                return "Unknown";
+        }
+    }
+
+    public function setStructureName(string $name): void {
+        $this->structureName = $name;
+    }
+
+    public function setRedstoneSaveMode(int $mode): bool {
+        if ($mode >= 0) {
+            $this->structureRedstoneSaveMode = $mode;
+            return true;
+        }
+        return false;
+    }
 }
--- src/types/StructureSettings.php
+++ src/types/StructureSettings.php
@@ -1,35 +1,77 @@
 <?php
 
-/*
- * This file is part of BedrockProtocol.
- * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
- *
- * BedrockProtocol is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
-
 declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;
 
 use pocketmine\math\Vector3;
 
-class StructureSettings{
+class StructureSettings {
+    public string $paletteName;
+    public bool $ignoreEntities;
+    public bool $ignoreBlocks;
+    public bool $allowNonTickingChunks;
+    public BlockPosition $dimensions;
+    public BlockPosition $offset;
+    public int $lastTouchedByPlayerID;
+    public int $rotation;
+    public int $mirror;
+    public int $animationMode;
+    public float $animationSeconds;
+    public float $integrityValue;
+    public int $integritySeed;
+    public Vector3 $pivot;
 
-	public string $paletteName;
-	public bool $ignoreEntities;
-	public bool $ignoreBlocks;
-	public bool $allowNonTickingChunks;
-	public BlockPosition $dimensions;
-	public BlockPosition $offset;
-	public int $lastTouchedByPlayerID;
-	public int $rotation;
-	public int $mirror;
-	public int $animationMode;
-	public float $animationSeconds;
-	public float $integrityValue;
-	public int $integritySeed;
-	public Vector3 $pivot;
+    public function __construct(
+        string $paletteName = "",
+        bool $ignoreEntities = false,
+        bool $ignoreBlocks = false,
+        bool $allowNonTickingChunks = false,
+        BlockPosition $dimensions = null,
+        BlockPosition $offset = null,
+        int $lastTouchedByPlayerID = 0,
+        int $rotation = 0,
+        int $mirror = 0,
+        int $animationMode = 0,
+        float $animationSeconds = 0.0,
+        float $integrityValue = 1.0,
+        int $integritySeed = 0,
+        Vector3 $pivot = null
+    ) {
+        $this->paletteName = $paletteName;
+        $this->ignoreEntities = $ignoreEntities;
+        $this->ignoreBlocks = $ignoreBlocks;
+        $this->allowNonTickingChunks = $allowNonTickingChunks;
+        $this->dimensions = $dimensions ?? new BlockPosition(0, 0, 0);
+        $this->offset = $offset ?? new BlockPosition(0, 0, 0);
+        $this->lastTouchedByPlayerID = $lastTouchedByPlayerID;
+        $this->rotation = $rotation;
+        $this->mirror = $mirror;
+        $this->animationMode = $animationMode;
+        $this->animationSeconds = $animationSeconds;
+        $this->integrityValue = $integrityValue;
+        $this->integritySeed = $integritySeed;
+        $this->pivot = $pivot ?? new Vector3(0, 0, 0);
+    }
+
+    public function isValidRotation(): bool {
+        return in_array($this->rotation, [0, 90, 180, 270], true);
+    }
+
+    public function isValidMirror(): bool {
+        return in_array($this->mirror, [0, 1], true);
+    }
+
+    public function setIntegrity(float $value): void {
+        $this->integrityValue = max(0.0, min(1.0, $value));
+    }
+
+    public function setPaletteName(string $name): void {
+        $this->paletteName = $name;
+    }
+
+    public function setAnimation(float $seconds, int $mode): void {
+        $this->animationSeconds = $seconds;
+        $this->animationMode = $mode;
+    }
 }
